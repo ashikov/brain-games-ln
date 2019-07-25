bc4ba234f1401d0478be9b0087bbfde97f90f79a
Roman Ashikov <rmn.shkv@gmail.com>
2019-07-25 10:07:17 +0700

    fixed cognitive complexity in isPrime function

--- src/games/primeGameData.js
+++ src/games/primeGameData.js
@@ -4,16 +4,13 @@ import engine from '../engine';
 const showDescription = () => console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
 
 const isPrime = (number) => {
-  if (number === 1 || number === 2) {
+  if (number === 2) {
     return true;
-  }
-  if (number % 2 === 0 || number % 3 === 0) {
-    return false;
   }
 
   const division = Math.floor(Math.sqrt(number));
 
-  for (let divisor = 3; divisor <= division; divisor += 1) {
+  for (let divisor = 2; divisor <= division; divisor += 1) {
     if (number % divisor === 0) {
       return false;
     }
